0d83e9bd17976ac6097bdeed44e0269b8b7da46c
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2021-03-14 08:54:29 -0400

    initial

--- Patch/SystemRenamer.cs
+++ Patch/SystemRenamer.cs
@@ -22,7 +22,7 @@ namespace SystemRenamer
         {
             public static void Postfix(SimGameState __instance, int timeLapse)
             {
-            if (__instance.CompanyTags.Contains(ModInit.Settings.TriggerTag1))
+                if (__instance.CompanyTags.Contains(ModInit.Settings.TriggerTag1))
                 {
                     StarSystem modSystem = __instance.StarSystems.Find(StarSystem => StarSystem.Name == ModInit.Settings.TargetSystemName);
                     Traverse.Create(modSystem).Property("Name").SetValue(__instance.Commander.LastName+"'s Hole");
